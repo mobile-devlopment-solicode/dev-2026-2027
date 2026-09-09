99602af760bd2fb7187adcd2baaf29cc1479ac92
Oumaima <mahfoudeoumaima@gmail.com>
2026-09-09 09:27:10 +0100

    base donnes

--- Algorithme/échanger-deux-variable.js
+++ Algorithme/échanger-deux-variable.js
@@ -1,14 +1,11 @@
 let a = 10;
 let b = 20;
-
 console.log("Avant échange :");
 console.log("a =", a);
 console.log("b =", b);
-
 let temp = a;
 a = b;
 b = temp;
-
 console.log("Après échange :");
 console.log("a =", a);
 console.log("b =", b);
