88976d16dc718d2cffb2abd0a2fb10f08f996a90
Oumaima <mahfoudeoumaima@gmail.com>
2026-09-10 15:02:30 +0100

    ..

--- Algorithme/node_repetitions.js
+++ Algorithme/node_repetitions.js
@@ -1,12 +1,22 @@
-let nombres = [4, 7, 2, 7, 9, 4, 5];
-let arr= [];
-for(let i=0; i< nombres.length; i++){
-    for(let j=0; j< nombres.length; j++){
-        if(i!== j &&nombres[i]===nombres[j]){
-            if(!arr.includes(nombres[i])) {
-                arr.push(nombres[i]);
-            }
+let nm=[4, 7, 2, 7, 9, 4, 5];
+let rep=[];
+for (let i = 0; i < nm.length; i++) {
+    let compteur = 0;
+    for (let j = 0; j < nm.length; j++) {
+        if (i !== j && nm[i] === nm[j]) {
+            compteur++;
         }
     }
+    if (compteur > 0) {
+        let dejaExiste = false;
+        for (let k = 0; k < rep.length; k++) {
+            if (rep[k] === nm[i]) {
+                dejaExiste = true;
+            }
+        }
+        if (dejaExiste === false) {
+            rep.push(nm[i]);
+        }   
+    }
 }
-console.log(arr);
+console.log(rep);
